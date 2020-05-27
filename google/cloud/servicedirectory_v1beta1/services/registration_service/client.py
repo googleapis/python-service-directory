cc6151bab63f91fbc7e8fe8b907aebcbba6e446f
yoshi-automation <yoshi-automation@google.com>
2020-05-27 11:30:58 -0700

    feat: implement multiple version for nodejs lib (#504)
    * feat: synthool generate src/index for nodejs client library
    
    * fix the linters
    
    * merge upstream
    
    * remove '-' in jinja tempalte
    
    * rename the functions
    
    * add try, finally to avoid change direcory
    
    * optional for versions and default verison
    
    Source-Author: Summer Ji <summerji@google.com>
    Source-Date: Fri May 1 09:29:52 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: cdddf139b36000b3a7c65fd2a7781e253262359a
    Source-Link: https://github.com/googleapis/synthtool/commit/cdddf139b36000b3a7c65fd2a7781e253262359a

--- google/cloud/servicedirectory_v1beta1/services/registration_service/client.py
+++ google/cloud/servicedirectory_v1beta1/services/registration_service/client.py
@@ -173,22 +173,6 @@ class RegistrationServiceClient(metaclass=RegistrationServiceClientMeta):
         return m.groupdict() if m else {}
 
     @staticmethod
-    def namespace_path(project: str, location: str, namespace: str) -> str:
-        """Return a fully-qualified namespace string."""
-        return "projects/{project}/locations/{location}/namespaces/{namespace}".format(
-            project=project, location=location, namespace=namespace
-        )
-
-    @staticmethod
-    def parse_namespace_path(path: str) -> Dict[str, str]:
-        """Parse a namespace path into its component segments."""
-        m = re.match(
-            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/namespaces/(?P<namespace>.+?)$",
-            path,
-        )
-        return m.groupdict() if m else {}
-
-    @staticmethod
     def service_path(project: str, location: str, namespace: str, service: str) -> str:
         """Return a fully-qualified service string."""
         return "projects/{project}/locations/{location}/namespaces/{namespace}/services/{service}".format(
@@ -200,6 +184,22 @@ class RegistrationServiceClient(metaclass=RegistrationServiceClientMeta):
         """Parse a service path into its component segments."""
         m = re.match(
             r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/namespaces/(?P<namespace>.+?)/services/(?P<service>.+?)$",
+            path,
+        )
+        return m.groupdict() if m else {}
+
+    @staticmethod
+    def namespace_path(project: str, location: str, namespace: str) -> str:
+        """Return a fully-qualified namespace string."""
+        return "projects/{project}/locations/{location}/namespaces/{namespace}".format(
+            project=project, location=location, namespace=namespace
+        )
+
+    @staticmethod
+    def parse_namespace_path(path: str) -> Dict[str, str]:
+        """Parse a namespace path into its component segments."""
+        m = re.match(
+            r"^projects/(?P<project>.+?)/locations/(?P<location>.+?)/namespaces/(?P<namespace>.+?)$",
             path,
         )
         return m.groupdict() if m else {}
