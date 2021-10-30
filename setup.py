caf0926889d0d20e8f605bdd0f3f5daa4c7a9c46
Anthonios Partheniou <partheniou@google.com>
2021-10-30 23:27:42 +0000

    fix(deps): drop packaging dependency

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setuptools.setup(
         "google-api-core >= 1.28.0, <3.0.0dev",
         "proto-plus >= 1.10.0",
         "grpc-google-iam-v1",
-        "packaging >= 14.3",
     ),
     python_requires=">=3.6",
     classifiers=[
